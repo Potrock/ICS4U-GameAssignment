123a21cfe4e5e2c3e2de5b378874aa3d3307286e
k e v i n <kevinkwan@crescentschool.org>
2018-01-31 17:51:00 -0500

    Update Gameboard.java
    commented Gameboard class

--- src/battleship/Gameboard.java
+++ src/battleship/Gameboard.java
@@ -3,8 +3,10 @@ package battleship;
 import ttt.I_CommonGame;
 
 public class Gameboard implements I_CommonGame {
+    // creates a string array to store the characters of the gameboard
     String[][] board = new String[BOARD_SIZE_Y][BOARD_SIZE_X];
-
+    
+    // sets the value of each position in the string array to a ~ character
     public Gameboard() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
